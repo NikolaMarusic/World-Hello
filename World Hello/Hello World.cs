d712a474d2434205c8d4abe2c627aa8bcd1c3185
NikolaMarusic <m.niko@live.se>
2017-01-19 11:10:51 +0100

    added code

---
+++ World Hello/Hello World.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace World_Hello
+{
+    class Hello_World
+    {
+        public void runCode()
+        {
+            Console.WriteLine();
+        }
+    }
+}
---
+++ World Hello/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace World_Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hello_World run = new Hello_World();
+            run.runCode();
+
+        }
+    }
+}
